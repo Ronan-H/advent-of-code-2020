a5b749745f3ff76664fb0d200b8ef01d328ba291
ronan <g00342393@gmit.ie>
2020-12-02 20:45:26 +0000

    Solved Day 2: Part 2

--- day-2/part-2.py
+++ day-2/part-2.py
@@ -8,11 +8,11 @@ num_valid = 0
 for line in lines:
     parts = line.split()
     policy_letter = parts[1][0]
-    min_letter, max_letter = [int(n) for n in parts[0].split('-')]
+    indexes = set(int(n) - 1 for n in parts[0].split('-'))
     password = parts[2]
-    num_policy_letters = sum(1 if c == policy_letter else 0 for c in password)
+    num_policy_letters = sum(1 if c == policy_letter and i in indexes else 0 for i, c in enumerate(password))
 
-    if min_letter <= num_policy_letters <= max_letter:
+    if num_policy_letters == 1:
         num_valid += 1
 
 print('Num valid:', num_valid)
