728dd63f7c3b97f925b23ade3f027c0b9d263cc7
ronan <g00342393@gmit.ie>
2020-12-05 12:44:17 +0000

    Solved Day 5: Part 2

--- day-5/part-2.py
+++ day-5/part-2.py
@@ -14,14 +14,16 @@ def binary_search(dirs, lower, upper):
         return binary_search(dirs[1:], lower + ceil((upper - lower) / 2), upper)
 
 
-highest = -1
+seat_ids = []
 with open('./input') as input_file:
     for line in input_file:
         dirs = line[:-1]  # excludes newline
         row = binary_search(dirs[:-3], 0, 127)
         col = binary_search(dirs[-3:], 0, 7)
         seat_id = (row * 8) + col
-        highest = max(highest, seat_id)
+        seat_ids.append(seat_id)
     input_file.close()
 
-print(highest)
+seat_ids.sort()
+my_seat = next(a + 1 for a, b in zip(seat_ids, seat_ids[1:]) if b - a == 2)
+print(my_seat)
